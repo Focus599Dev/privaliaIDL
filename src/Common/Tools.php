884d9f07c1b086fcbc2499fa4992864a69a66ab2
Marlon oliveira barbosa <marlon.academi@gmail.com>
2019-11-29 18:08:08 -0300

    Update Tools.php

--- src/Common/Tools.php
+++ src/Common/Tools.php
@@ -5,15 +5,27 @@ namespace Focus599Dev\privaliaIDL\Common;
 class Tools {
 
 	protected $url = array(
-		1 => 'http://177.126.188.77/WSPrivalia/wsPrivalia.php',
-		2 => 'http://177.126.188.66/WSPrivalia/wsPrivalia.php'
+		'0550' => array(
+			1 => 'http://177.126.188.77/WSPrivalia/wsPrivalia.php',
+			2 => 'http://177.126.188.66/WSPrivalia/wsPrivalia.php'
+		),
+		'0530' => array(
+			1 => 'http://wsprivalia.sequoialog.com.br:8895',
+			2 => 'http://wsprivalia.sequoialog.com.br:8895'
+		)
 	);
 
 	protected $uri = array(
-		1 => 'http://177.126.188.77/WSPrivalia',
-		2 => 'http://177.126.188.66/WSPrivalia'
+		'0550' => array(
+			1 => 'http://177.126.188.77/WSPrivalia',
+			2 => 'http://177.126.188.66/WSPrivalia'
+		),
+		'0530' => array(
+			1 => 'http://wsprivalia.sequoialog.com.br:8895',
+			2 => 'http://wsprivalia.sequoialog.com.br:8895'
+		)
 	);
-
+	
 	protected $tAmb = 2;
 
 	public $request;
